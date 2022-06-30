a0232cb530b8b58779ab62201907ab4d88e1b237
chenqinhao <qhchen96@gmail.com>
2022-06-30 23:00:24 +0800

    搭建途觅的脚手架

---
+++ tome-framework/tome-catchlog-starter/src/main/java/com/khetao/tome/catchlog/CatchLogAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.khetao.tome.catchlog;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configurable
+@EnableAspectJAutoProxy
+public class CatchLogAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(CatchLogAspect.class)
+    public CatchLogAspect catchLogAspect() {
+        return new CatchLogAspect();
+    }
+
+}
