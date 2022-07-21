f19038248de1a23cf383ecd856ce67c54d957218
chenqinhao <qhchen96@gmail.com>
2022-07-21 23:32:16 +0800

    管理员服务rbac初步代码

---
+++ tome-service-manager/tome-service-manager-domain/src/main/java/com/khetao/tome/manager/domain/Department.java
@@ -0,0 +1,15 @@
+package com.khetao.tome.manager.domain;
+
+import lombok.Data;
+
+/**
+ * 部门
+ * @author chenqinhao 2022/7/21
+ * @email qhchen96@gmail.com
+ */
+@Data
+public class Department {
+    private Long departmentId;
+    private String departmentName;
+
+}
---
+++ tome-service-manager/tome-service-manager-domain/src/main/java/com/khetao/tome/manager/domain/Permit.java
@@ -0,0 +1,15 @@
+package com.khetao.tome.manager.domain;
+
+import lombok.Data;
+
+/**
+ * 权限
+ * @author chenqinhao 2022/7/21
+ * @email qhchen96@gmail.com
+ */
+@Data
+public class Permit {
+    private Long permitId;
+    private String permitName;
+    private String permitCode;
+}
